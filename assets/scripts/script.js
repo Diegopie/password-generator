0cc36482e65533bce7f733091513ec99ef235ee3
Diego Hernandez <ishtaracademy@outlook.com>
2020-08-05 10:56:39 -0600

    Created prompts and confirms to get user variables

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,14 +1,51 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+  console.log(generateBtn)
+
+// Do I need arrays for each condition?????????
+const specChar = ['!', '#', '$', '%', '&', '*', '=', '+', '-', '/', ':', ';', '<',  '>', '?', '@', '[',']', '^', '_', '`', '{', '}', '~'];
+
+
+
+// Store User Variables  - store in an object?
+  // length of characters
+var userLength = prompt('Please choose the length of your password, between 8 - 128 characters');
+  console.log(userLength);
+  // include special characters?
+var inclSpec = confirm('Would you like to include special characters?');
+  console.log("incSpec " + inclSpec);
+  // include numbers
+var inclNumb = confirm('Would you like to include numbers?');
+  console.log("incNumb " + inclNumb);
+var inclLower = confirm('Would you like to include lower-case letters?');
+  console.log("incLower " + inclLower);
+
+
+
+
+  
+console.log("2nd " + userLength)
+function generatePassword () {
+  // code to be run
+  
+  return 1+2
+}
+  console.log("generatePassword is runing");
+
+
+generatePassword();
+
 
 // Write password to the #password input
 function writePassword() {
+  console.log("writePassword is running")
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
