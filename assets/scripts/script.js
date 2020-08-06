8b9a51bb9a797b2d932358106d67ff4d30d168fe
Diego Hernandez <ishtaracademy@outlook.com>
2020-08-06 14:22:38 -0600

    I nested all the arrays into a user choices array. I have working code to generate pass with two variables

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,10 +3,15 @@ var generateBtn = document.querySelector("#generate");
   console.log(generateBtn)
 
 // Array for User Choices
+
 const specChar = ['!', '#', '$', '%', '&', '*', '=', '+', '-', '/', ':', ';', '<',  '>', '?', '@', '[',']', '^', '_', '`', '{', '}', '~']; //length is 24
 const numbChar = ['1', '2', '3', '4', '5', '6', '7', '8', '9']; //length is 9
 const lowerCase = ['a', 'b',' c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k' , 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; //length is 26
 const upperCase = ['A', 'B',' C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K' , 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']; //length is 26
+var userChoices = [specChar, numbChar, lowerCase, upperCase]
+
+console.log(userChoices)
+
  
 
 // Store User Variables in an array
@@ -23,30 +28,33 @@ function generatePassword () {
   var userLength = prompt('Please choose the length of your password, between 8 - 128 characters');
 
   //Conditional to check if length is correct
-  if (userLength >= 8 && userLength <= 128) {
+  if (userLength < 8 || userLength > 128) {
+    alert("You must choose a number between 8 - 128. Please try again.")
+
+  } else if (userLength >= 8 && userLength <= 128) {
       console.log(parseInt(userLength));
       //Log user selection to userSelection
       userSelection.push(parseInt(userLength));
 
-    // include special characters?
-    var inclSpec = confirm('Would you like to include special characters?');
-      console.log("incSpec " + inclSpec);
-      //Log user selection to userSelection
-      userSelection.push(inclSpec);
+      // include special characters?
+      var inclSpec = confirm('Would you like to include special characters?');
+        console.log("incSpec " + inclSpec);
+        //Log user selection to userSelection
+        userSelection.push(inclSpec);
 
-    // include numbers
-    var inclNumb = confirm('Would you like to include numbers?');
-      console.log("incNumb " + inclNumb);
-      //Log user selection to userSelection
-      userSelection.push(inclNumb);
-      
-    // include lower case
-    var inclLower = confirm('Would you like to include lower-case letters?');
-      console.log("incLower " + inclLower);
-      //Log user selection to userSelection
-      userSelection.push(inclLower);
+      // include numbers
+      var inclNumb = confirm('Would you like to include numbers?');
+        console.log("incNumb " + inclNumb);
+        //Log user selection to userSelection
+        userSelection.push(inclNumb);
+        
+      // include lower case
+      var inclLower = confirm('Would you like to include lower-case letters?');
+        console.log("incLower " + inclLower);
+        //Log user selection to userSelection
+        userSelection.push(inclLower);
 
-    console.log(userSelection);
+      console.log(userSelection);
   }
 
 
@@ -56,14 +64,51 @@ function generatePassword () {
 
 // Combine user selections to generate the password 
    
-      var pass = '';
-      for (i = 0; i < userLength; i++) {
-        if (inclSpec) {
-          var specNum = Math.floor(Math.random() * specChar.length) + 1;
-            console.log("random index value " + specChar[specNum]);
-          pass += specChar[specNum]
-          console.log(pass)   
-      }
+
+
+
+
+
+
+
+
+
+
+// 2nd draft of creating random password with two variables
+
+  var pass = '';
+  for (i = 0; pass.length < userLength; i++) {
+    if (inclSpec && inclNumb) {
+      var specNum = Math.floor(Math.random() * specChar.length);
+        console.log("random specIndex value " + specChar[specNum]);
+      var numbNum = Math.floor(Math.random() * numbChar.length);
+        console.log("random specIndex value " + numbChar[numbNum]);
+      pass += specChar[specNum];
+      pass += numbChar[numbNum];
+      console.log("pass string length" + pass.length)  
+      console.log('pass type' + typeof pass.length) 
+  } else if (inclNumb) {
+      var numbNum = Math.floor(Math.random() * numbChar.length);
+      console.log("RNG " + numbNum)
+        console.log("random numbIndex value " + numbChar[numbNum]);
+      pass += numbChar[numbNum];
+  
+      
+  }
+
+
+
+
+
+      // 1st draft of creating random password
+      // var pass = '';
+      // for (i = 0; i < userLength; i++) {
+      //   if (inclSpec) {
+      //     var specNum = Math.floor(Math.random() * specChar.length) + 1;
+      //       console.log("random index value " + specChar[specNum]);
+      //     pass += specChar[specNum]
+      //     console.log(pass)   
+      // }
     
 
 
