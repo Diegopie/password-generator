0e5740f79702bbee1fac75c109c069fa03f2ddee
Diego Hernandez <ishtaracademy@outlook.com>
2020-08-07 12:05:05 -0600

    I have created a dynamic blank string that becomes populated as users select their options. I now need to refactor the rng system to use that dynamic string

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,93 +1,107 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-  console.log(generateBtn)
+
 
 // Array for User Choices
-
-const specChar = ['!', '#', '$', '%', '&', '*', '=', '+', '-', '/', ':', ';', '<',  '>', '?', '@', '[',']', '^', '_', '`', '{', '}', '~']; //length is 24
-const numbChar = ['1', '2', '3', '4', '5', '6', '7', '8', '9']; //length is 9
-const lowerCase = ['a', 'b',' c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k' , 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; //length is 26
-const upperCase = ['A', 'B',' C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K' , 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']; //length is 26
+let specChar = '!#$%&*=+-/:;<>?@[]^_`{}~'; //length is 24
+let numbChar = ['1', '2', '3', '4', '5', '6', '7', '8', '9']; //length is 9
+let lowerCase = ['a', 'b','c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; //length is 26
+let upperCase = ['A', 'B','C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']; //length is 26
+// this array holds User Choises Arrays so that an RNG system can access every arrray at random and every array element at random
 var userChoices = [specChar, numbChar, lowerCase, upperCase];
 
-console.log('user choices array' + userChoices);
+console.log('user choices array: ' + userChoices);
 
- console.log(userChoices[0][14])
-
-// Store User Variables in an array
-  
-var userSelection = []; //could i create variables that convert a true into 
-  
-
-function generatePassword () {
-// code to be run
+// Store User Selected boolian values in an array, userArray will then have user true statements stored in here for the genPass() to use
+var userSelection = []; 
+var userArray = ''; //store their selected options in a new array?
+console.log('blank userarry' + userArray)
 
 //Get conditions from user
-
-  // length of characters
+function generatePassword () {
+  // Ask length of characters from user
   var userLength = prompt('Please choose the length of your password, between 8 - 128 characters');
-
   //Conditional to check if length is correct
   if (userLength < 8 || userLength > 128) {
     alert("You must choose a number between 8 - 128. Please try again.")
-
+    // If correct, get the rest of their choices and log conditions from user
   } else if (userLength >= 8 && userLength <= 128) {
-      console.log('userLength' + parseInt(userLength));
+          console.log('userLength: ' + parseInt(userLength));
+    // Ask user to include special characters:
+    var inclSpec = confirm('Would you like to include special characters?');
+          console.log("incSpec: " + inclSpec);
       //Log user selection to userSelection
-      userSelection.push(parseInt(userLength));
+      userSelection.push(inclSpec);
+      // If user selected true, send this array to the userArray string
+      if (inclSpec) {
+        userArray += specChar
+      }
+          console.log('1st conditional userArray: ' + userArray)
+    // Ask user to include numbers:
+    var inclNumb = confirm('Would you like to include numbers?');
+          console.log("incNumb: " + inclNumb);
+      //Log user selection to userSelection
+      userSelection.push(inclNumb);
+      // If user selected true, send this array to the userArray string
+      if (inclNumb) {
+        userArray += numbChar
+      }
+          console.log('2st conditional userArray: ' + userArray)
+    // Ask user to include lower case?
+    var inclLower = confirm('Would you like to include lower-case letters?');
+          console.log("incLower: " + inclLower);
+      //Log user selection to userSelection
+      userSelection.push(inclLower);
+      // If user selected true, send this array to the userArray string
+      if (inclLower) {
+        userArray += lowerCase
+      }
+          console.log('3rd conditional userArray: ' + userArray)
+    // Ask user to include Upper case?
+    var incUpper = confirm('Would you like to include Upper-case letters?');
+          console.log("incUpper: " + incUpper);
+      //Log user selection to userSelection
+      userSelection.push(incUpper);
+      // If user selected true, send this array to the userArray string
+      if (incUpper) {
+        userArray += upperCase
+      }
+      
 
-      // include special characters?
-      var inclSpec = confirm('Would you like to include special characters?');
-        console.log("incSpec " + inclSpec);
-        //Log user selection to userSelection
-        userSelection.push(inclSpec);
-
-      // include numbers
-      var inclNumb = confirm('Would you like to include numbers?');
-        console.log("incNumb " + inclNumb);
-        //Log user selection to userSelection
-        userSelection.push(inclNumb);
-        
-      // include lower case
-      var inclLower = confirm('Would you like to include lower-case letters?');
-        console.log("incLower " + inclLower);
-        //Log user selection to userSelection
-        userSelection.push(inclLower);
-
-      console.log(userSelection);
+    console.log('userSelection Array: ' + userSelection); //monitor user selection
   }
 
 
-
+  console.log('This is the final user array: ' + userArray)
   console.log("-----------------");
-  console.log('userSelectionuser Array' + Selection);
 
-// Combine user selections to generate the password 
-   
-var pass = '';
-var index = 0;
-for (i = 0; pass.length < userLength; i++) {
-  if (inclSpec && inclNumb) {
-    // Create a random index #, so that the order of the password is also random??
+// Declair var to be used to generate and display password
+var pass = "";
 
-    var arrayNumb = Math.floor(Math.random() * userChoices.length);
-    console.log('array number ' + arrayNumb)
-    //create a random index number to get an element from the array
-    var indexNumb = Math.floor(Math.random() * userChoices[arrayNumb].length);
-    console.log('index number' + indexNumb)
-    console.log('this should be the array item ' + userChoices[arrayNumb][indexNumb])
-    pass += userChoices[arrayNumb][indexNumb];
-    
-//     console.log("pass string length" + pass.length)  
-//     console.log('pass type' + typeof pass.length) 
-// } else if (inclNumb) {
-//     var numbNum = Math.floor(Math.random() * numbChar.length);
-//     console.log("RNG " + numbNum)
-//       console.log("random numbIndex value " + numbChar[numbNum]);
-//     pass += numbChar[numbNum];
+// Run a function, based on the values created by the user 
+function genPass (spec, numb, upper, lower) {
+  for (i = 0; pass.length < userLength; i++) {
+    console.log('Acess dynamic userSelection: ' + userSelection[0], userSelection[1], userSelection[2], userSelection[3])
+    if (userSelection[0] && userSelection[1] && userSelection[2] && userSelection[3]) {
+      // Create a random index #, so that the order of the password is also random??
 
+      var arrayNumb = Math.floor(Math.random() * userChoices.length);
+      console.log('arrayNumber: ' + arrayNumb);
+        console.log('-----------------------------');
+      //create a random index number to get an element from the array
+      var indexNumb = Math.floor(Math.random() * userChoices[arrayNumb].length);
+      console.log('indexNumber: ' + indexNumb);
+        console.log('-----------------------------');
+      console.log('array item: ' + userChoices[arrayNumb][indexNumb]);
+        console.log('-----------------------------');
+      pass += userChoices[arrayNumb][indexNumb];
+      console.log('show current pass: ' + pass);
+    }      
   }
 }
+
+genPass(userSelection[0], userSelection[1], userSelection[2], userSelection[3]); 
+
 
 
 
